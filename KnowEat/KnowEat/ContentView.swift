7d12a5ac31eabbdb7863b5fd921907a2acc3be98
Marian hernandez <marian.hernandez@ingenieria.unam.edu>
2026-02-19 23:36:49 +0100

    chore(know-eat): hello world from KnowEat

---
+++ KnowEat/KnowEat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KnowEat
+//
+//  Created by Lisette HG on 19/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KnowEat/KnowEat/KnowEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowEatApp.swift
+//  KnowEat
+//
+//  Created by Lisette HG on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct KnowEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
